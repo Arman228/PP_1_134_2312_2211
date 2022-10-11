bf430bf875a52704a8431b3e642b6c88f9efa89a
Arman228 <arman.treyder@bk.ru>
2022-10-11 13:45:42 +0300

    erere

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -12,24 +12,24 @@ import java.util.List;
 
 public class Main {
 
-       public static void main(String[] args) {
-           Util util=new Util();
-           util.getConnection();
-          // UserDao userDao = new UserDaoJDBCImpl();
+    public static void main(String[] args) {
+        Util util = new Util();
+        util.getConnection();
+        UserDao userDao = new UserDaoJDBCImpl();
 
-           //userDao.createUsersTable();
+        userDao.createUsersTable();
 
-             //userDao.saveUser("Name1", "LastName1", (byte) 20);
-         // userDao.saveUser("Name2", "LastName2", (byte) 25);
-          //  userDao.saveUser("Name3", "LastName3", (byte) 31);
-         //  userDao.saveUser("Name4", "LastName4", (byte) 38);
-//System.out.println(userDao);
-          // userDao.removeUserById(1);
-          //userDao.getAllUsers();
-          // userDao.cleanUsersTable();
-          // userDao.dropUsersTable();
-       }
+        userDao.saveUser("Name1", "LastName1", (byte) 20);
+         userDao.saveUser("Name2", "LastName2", (byte) 25);
+          userDao.saveUser("Name3", "LastName3", (byte) 31);
+          userDao.saveUser("Name4", "LastName4", (byte) 38);
+System.out.println(userDao);
+         userDao.removeUserById(1);
+        userDao.getAllUsers();
+         userDao.cleanUsersTable();
+         userDao.dropUsersTable();
     }
+}
 
 
 
